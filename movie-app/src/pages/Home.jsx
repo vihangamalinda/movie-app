d96974c044f81c6ba10511f109ad75c359b9c0e6
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-18 23:10:18 -0800

    feat:adding use-state.

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,24 +1,32 @@
 import MovieCard from "../components/MovieCard.jsx";
+import {useState} from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies =[
-        {id:1,title:"Movie 1",release_date:"444",url:"mmm"},
-        {id:2,title:"Movie 2",release_date:"222",url:"mmm"},
-        {id:3,title:"Movie 3",release_date:"333",url:"mmm"},
+        {id:1,title:"Red",release_date:"444",url:"mmm"},
+        {id:2,title:"Hoy hat",release_date:"222",url:"mmm"},
+        {id:3,title:"Jhonnn as",release_date:"333",url:"mmm"},
 
     ]
     const handler = (e)=>{
         e.preventDefault();
+        console.log(searchQuery);
+        setSearchQuery("");
     }
 
     return (
         <div className="home">
             <form onSubmit={handler} className="search-form">
-                <input type="text" placeholder="Search..."  className="search-input"/>
+                <input type="text" placeholder="Search..."  className="search-input" value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
                 <button type="submit">Search</button>
             </form>
             <div className="movies-grid">
-                {movies.map((movie) => <MovieCard movie={movie} key={movie.id}/>)}
+                { movies.map((movie) =>
+                    (<MovieCard movie={movie} key={movie.id}/>))}
             </div>
         </div>
     )
