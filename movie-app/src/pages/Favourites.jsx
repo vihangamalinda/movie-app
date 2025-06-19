ec5b8252815ce5d24efa9ec23bb5954b07a320b5
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-19 03:18:02 -0800

    feat: Adding sample code for favourite section

---
+++ movie-app/src/pages/Favourites.jsx
@@ -0,0 +1,9 @@
+function Favourites(){
+    return (
+        <div className="favourites-empty">
+            <h2>No favourites were added</h2>
+        </div>
+    )
+}
+
+export default Favourites;
