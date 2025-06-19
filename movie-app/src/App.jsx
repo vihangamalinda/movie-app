48207195bb541c6d42ddb9b1cc13774a9d4897d1
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-19 03:27:30 -0800

    feat: Adding routing related logic.

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 import Home from './pages/Home'
+import {Routes,Route} from "react-router-dom";
+import Favourites from "./pages/Favourites.jsx";
 function App() {
   return (
     <>
-      <Home/>
+      <main className="main-content">
+          <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/favourites" element={<Favourites/> }/>
+          </Routes>
+      </main>
      </>
   )
 }
