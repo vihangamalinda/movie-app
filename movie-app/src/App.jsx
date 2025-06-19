6f06a27bcb34bc608408f3f784efe9367d77226d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-19 03:46:52 -0800

    feat: Integrating the nav-bar.

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -2,16 +2,18 @@ import './App.css'
 import Home from './pages/Home'
 import {Routes,Route} from "react-router-dom";
 import Favourites from "./pages/Favourites.jsx";
+import NavBar from "./components/NavBar.jsx";
 function App() {
   return (
-    <>
-      <main className="main-content">
-          <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/favourites" element={<Favourites/> }/>
-          </Routes>
-      </main>
-     </>
+      <div>
+          <NavBar/>
+          <main className="main-content">
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/favourites" element={<Favourites/>}/>
+              </Routes>
+          </main>
+      </div>
   )
 }
 
